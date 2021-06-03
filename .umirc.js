5874ca56fa9a1dd19d9a007bb4232e35a4b87654
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-03 14:39:20 +0700

    refactor: revert changes

--- .umirc.js
+++ .umirc.js
@@ -8,7 +8,7 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 // how to speed compile: https://umijs.org/guide/boost-compile-speed
 export default {
   // IMPORTANT! change next line to yours or delete. And hide in dev
-  publicPath: '/',
+  publicPath: isDevelopment ? '/' : 'https://cdn.antd-admin.zuiidea.com/',
   alias: {
     api: resolve(__dirname, './src/services/'),
     components: resolve(__dirname, './src/components'),
